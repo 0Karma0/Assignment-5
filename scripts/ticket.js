20220afcb6ef6458b1a8790d1beadba2959c5168
mahade <mahade.adib45@gmail.com>
2024-02-19 15:49:56 +0600

    added few functions

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -1,5 +1,42 @@
+const allBtn = document.getElementsByClassName('btn');
+let count = 0
+let seats = 8
+
+for (const btn of allBtn){
+    btn.addEventListener('click', function(e){
+        count = count + 1;
+
+        setInnerText('selectedCount', count)
+        seats = seats - 1;
+        setInnerText('left', seats);
+
+        const seatName = e.target.parentNode.childNodes[1].innerText;
+        console.log(e.target.parentNode.childNodes[1].innerText);
+        const price = 550;
+        const selectedContainer = document.getElementById("container");
+
+        const li = document.createElement("li");
+
+        const p = document.createElement("p");
+        p.innerText = seatName
+        const p2 = document.createElement("p2");
+        p2.innerText = price;
+
+        li.appendChild(p);
+        li.appendChild(p2);
+
+        selectedContainer.appendChild(li);
+
+        totalCost('total-cost', parseInt(price));
+
+        grandTotal('grand-total', parseInt(price));
+
+    })
+}
+
 function next(){
     hideElementId('head');
     hideElementId('main');
     showElementById('confirm');
 }
+
--- scripts/utility.js
+++ scripts/utility.js
@@ -7,3 +7,24 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden')
 }
+
+function setInnerText(id, value){
+    document.getElementById(id).innerText = value;
+}
+
+function totalCost(id, value){
+    const totalCost = document.getElementById(id).innerText;
+
+        const convertedTotalCost = parseInt(totalCost);
+        const sum = convertedTotalCost + parseInt(value);
+        setInnerText(id, sum);
+}
+
+function grandTotal(id, value){
+    const grandTotal = document.getElementById(id).innerText;
+        const convertedGrandTotal = parseInt(grandTotal);
+
+        const sum2 = convertedGrandTotal + parseInt(value);
+
+        setInnerText(id, sum2);
+}
